b97cc95fc9f044cd845df294166cd0ee5b990b86
Aleksei <odvansgame@gmail.com>
2023-01-06 20:07:11 +0200

    Initial commit

---
+++ src/main/java/ee/kolbaska/kolbaska/KolbaskaApplication.java
@@ -0,0 +1,13 @@
+package ee.kolbaska.kolbaska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolbaskaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolbaskaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/kolbaska/kolbaska/KolbaskaApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.kolbaska.kolbaska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolbaskaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
