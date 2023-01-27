0ee72ded987bbe60c2145925a2d601d18c733aee
Aleksei <odvansgame@gmail.com>
2023-01-27 13:24:27 +0200

    #184334430 Change api url

--- src/main/java/ee/kolbaska/kolbaska/controller/RestaurantController.java
+++ src/main/java/ee/kolbaska/kolbaska/controller/RestaurantController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/restaurant")
+@RequestMapping("${api.basepath}/restaurant")
 @RequiredArgsConstructor
 public class RestaurantController {
 
