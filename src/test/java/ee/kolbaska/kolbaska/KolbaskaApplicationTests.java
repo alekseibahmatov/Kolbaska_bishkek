983b370a597f066797d691871489c5ddf5e5aa36
Aleksei <albahm@ttu.ee>
2023-01-30 18:02:33 +0200

    Adder swagger ui and cors

--- src/test/java/ee/kolbaska/kolbaska/KolbaskaApplicationTests.java
+++ src/test/java/ee/kolbaska/kolbaska/KolbaskaApplicationTests.java
@@ -2,8 +2,10 @@ package ee.kolbaska.kolbaska;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/tests.properties")
 class KolbaskaApplicationTests {
 
     @Test
