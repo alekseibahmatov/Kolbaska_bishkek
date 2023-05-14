075d3b4e87bd2dca64ec5bc23bfa7cd7eb43d25b
alekseibahmatov <albahm@ttu.ee>
2023-05-14 14:01:11 +0300

    Update

--- script.sh
+++ script.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+
+# Start the gradle bootRun process in the background
+gradle bootRun -Pdebug &
+
+# Remember its PID
+pid=$!
+
 while true; do
-    ./gradlew --stop
-    ./gradlew -t build -x test &
-    ./gradlew bootRun -Pdebug
+  inotifywait -r -e modify,create,delete src
+
+  # Kill the old gradle bootRun process
+  kill $pid
+
+  # Start a new gradle bootRun process in the background
+  gradle bootRun -Pdebug &
+
+  # Remember its PID
+  pid=$!
 done
